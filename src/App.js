f6ef0e6a46d55f0fe5eaad2039257c166cba58dd
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-12 11:42:28 +0100

    Added app js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      Dictionary
+      <img
+        src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/981/original/DOnkey_Sonia_with_my_Apple.jpg?1706628660"
+        className="App-logo img-fluid"
+        alt="logo"
+      />
+    </div>
+  );
+}
+
+export default App;
